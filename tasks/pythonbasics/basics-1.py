bea89daeca54e7b3454afb328dbd11f326d349b6
scarecrow118 <yuchupil2024@itcollege.lviv.ua>
2025-10-25 14:55:19 +0300

    Add files via upload

---
+++ tasks/pythonbasics/basics-1.py
@@ -0,0 +1,16 @@
+
+a = "змінна з текстом"
+b = 1  
+b1 = 1.1  
+c = ["a", 1, 1.25, "Слово", a]  
+d = {"a": "Слово", "b": 1, a: b}  
+e = ("a", a)  
+f = {"ss", a + str(b)}  
+
+print("a =", a)
+print("b =", b)
+print("b1 =", b1)
+print("c =", c)
+print("d =", d)
+print("e =", e)
+print("f =", f)
